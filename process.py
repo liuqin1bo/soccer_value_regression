3586b5c91f27b07f466b7363de94d7901ec6edc8
刘勤波 <liuqinbo@xiaomi.com>
2019-11-15 11:59:44 +0800

    ddd

--- process.py
+++ process.py
@@ -2,6 +2,7 @@
 from utilities import *
 import os
 import shutil
+from pandas_profiling import ProfileReport
 import xgboost as xgb
 from xgboost import plot_importance
 from sklearn.preprocessing import Imputer
@@ -12,6 +13,7 @@ from sklearn.preprocessing import Imputer
 # @Desc  : Processing
 precode = True  ## 代码试验工作, 非运行代码
 submit_file_has_generated = False
+stats_file_has_generated = True
 
 pd.options.display.float_format = '{:,.2f}'.format
 pd.set_option('display.max_rows', 20)
@@ -22,7 +24,23 @@ pd.set_option('display.width', 1000)
 def load_data(fileName):
     return pd.read_csv(fileName, low_memory=False,
                        encoding='utf-8')
-
+def train_df_stats(train_df):
+    stats_dir = "./stats"
+    if  not stats_file_has_generated:
+        try:
+            # create the output Directory
+            if  os.path.exists(stats_dir):
+                shutil.rmtree(stats_dir)
+                print("Directory ", stats_dir, " Removed.")
+                os.mkdir(stats_dir)
+                print("Directory ", stats_dir, " Created.")
+            else:
+                os.mkdir(stats_dir)
+                print("Directory ", stats_dir, " Created.")
+        except FileExistsError:
+            print("Creation of the directory %s failed" % stats_dir)
+        data_stats_profile = train_df.profile_report(title='Soccer Player Dataset',style={'full_width': True})
+        data_stats_profile.to_file(stats_dir + '/data_stats_profile.html')
 
 
     
@@ -35,7 +53,7 @@ def get_train_feature_list(feature):
                                    'work_rate_att', 'work_rate_def', 'preferred_foot', 'crossing',
                                    'finishing', 'heading_accuracy', 'short_passing', 'volleys',
                                    'dribbling', 'curve', 'free_kick_accuracy', 'long_passing',
-                                   'ball_control', 'acceleration', 'sprint_speed', 'agility',
+                                   'acceleration', 'sprint_speed', 'agility',
                                    'reactions', 'balance', 'shot_power', 'jumping', 'stamina',
                                    'strength', 'long_shots', 'aggression', 'interceptions',
                                    'positioning', 'vision', 'penalties', 'marking', 'standing_tackle',
@@ -47,6 +65,9 @@ def get_train_feature_list(feature):
 def get_test_data_for_train(fileName, train_feature):
     df = pd.read_csv(fileName, low_memory=False,
                        encoding='utf-8')
+    today = date(2018, 1, 1)
+    df['birth_date'] = pd.to_datetime(df['birth_date']).dt.date
+    df['age'] = (today - df['birth_date']).apply(lambda x: x.days) / 365.0
     print(df[train_feature].head(5))  # 只是验证一下训练和测试的特征排序是否一致
     return df[train_feature].values
 
@@ -76,11 +97,20 @@ def train_and_test(X_train, Y_train, X_test):
         except FileExistsError:
             print("Creation of the directory %s failed" % output_dir)
         predict_df.to_csv("./output/submit.csv", index=None)
-    
+    plot_importance(model)
+    plt.show()
     
 if __name__ == '__main__':
     train_df = load_data("./data/train.csv")
+    # 获得球员年龄
+    today = date(2018, 1, 1)
+    train_df['birth_date'] = pd.to_datetime(train_df['birth_date']).dt.date
+    train_df['age'] = (today - train_df['birth_date']).apply(lambda x: x.days) / 365.0
     print(train_df.head(10))
+    print(train_df.info())
+    train_df_stats(train_df)  # 输出训练数据统计结果
+    
+
     if precode:
         feature = train_df.columns.values.tolist()
         print("feature is:\n", feature)
--- utilities.py
+++ utilities.py
@@ -1,3 +1,4 @@
+from datetime import date
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
